fc7c89f903060c53705521985218d06450d023ec
manikkaushal <manik.kaushal1@gmail.com>
2023-02-14 00:30:12 +0530

    Update TaskForm.js

--- Task Manager/Task-manager/src/components/TaskForm.js
+++ Task Manager/Task-manager/src/components/TaskForm.js
@@ -1,8 +1,13 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useContext,useState,useEffect } from 'react'
+
 import { TaskListContext } from '../contexts/TaskListContext'
 
+import uuid from 'uuid'
+
 const TaskForm = () => {
+
   const { addTask, clearList, editTask, editItem } = useContext(TaskListContext)
+
   const [title, setTitle] = useState('')
 
   const handleSubmit = e => {
@@ -13,6 +18,24 @@ const TaskForm = () => {
     } else {
       editTask(title, editItem.id)
     }
+      
+    const taskData= { title, id: uuid() };
+    
+    console.log(taskData);
+  
+
+    fetch(`http://localhost:8000/tasks`,{
+      method:"POST",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(taskData)
+    }).then((res)=>{
+      console.log(res.status)
+      alert('Saved successfully.')
+    
+    }).catch((err)=>{
+      console.log(err.message)
+    })    
+   
   }
 
   const handleChange = e => {
@@ -29,6 +52,7 @@ const TaskForm = () => {
   }, [editItem])
 
   return (
+    
     <form onSubmit={handleSubmit} className="form">
       <input
         type="text"
@@ -40,9 +64,9 @@ const TaskForm = () => {
       />
       <div className="buttons">
         <button type="submit" className="btn add-task-btn">
-          {editItem ? 'Edit Task' : 'Add Task'}
+       Add Task
         </button>
-        <button className="btn clear-btn" onClick={clearList}>
+        <button className="btn clear-btn" >
           Clear
         </button>
       </div>
